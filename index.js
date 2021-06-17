2225ddf7f54748dc8ca150c18ab0cd2d54847463
caqlayan <79770701+caqlayan@users.noreply.github.com>
2021-06-17 23:47:13 +0300

    Update index.js
    bugfix

--- index.js
+++ index.js
@@ -88,7 +88,7 @@ client.elevation = message => {
   let permlvl = 0;
   if (message.member.hasPermission("BAN_MEMBERS")) permlvl = 2;
   if (message.member.hasPermission("ADMINISTRATOR")) permlvl = 3;
-  if (message.author.id === ayarlar.sahip) permlvl = 4;
+  if (message.author.id === botsahip) permlvl = 4;
   return permlvl;
 };
 
